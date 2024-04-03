43395ddfc3cc86bf6a33f71024aeae012ce27ff0
Daphne O'Malley <domalle1@terpmail.umd.edu>
2024-04-03 18:14:33 -0400

    added collaborative_programming.py

--- collaborative_programming.py
+++ collaborative_programming.py
@@ -1,1 +1,12 @@
+"""  A music library enabling users to manage songs, playlists, and perform 
+        various functions using implemented code structures, supported by 
+        CSV data storage. """
 
+class Playlist:
+    """ Represents the music library where users can create their own playlist.
+    
+    Attributes:
+        song ():
+        playlist_name ():
+        sort_by ():
+    """
